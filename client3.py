b1ec2b702acc98afbd36efdf68d376e933068714
JagritiSah <94858845+JagritiSah@users.noreply.github.com>
2023-07-16 13:08:46 +0530

    JPFUpdate client3.py

--- client3.py
+++ client3.py
@@ -39,25 +39,6 @@ def getDataPoint(quote):
     price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
-# unit test
-def test getDataPoint_calculatePrice(self):
-    quotes =[
-        {'top task': {'price': 121.2, 'size':36}, 'timestamp': '2019=02-11 22:06:30.572453', 'top_bid': { 'price': 120.48,  'size': 109}, 'id': '0.109974697771', 'stock':'ABC'},
-        {'top task': {'price': 121.68, 'size':4}, 'timestamp': '2019=02-11 22:06:30.572453', 'top_bid': { 'price': 117.87,  'size': 81}, 'id': '0.109974697771', 'stock':'DEF'}
-    ]
-    for quote in quotes:
-        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],(quote['top_bid']['price'] + quote['top_ask']['price'])/2))
-
-# unit test
-def test getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes =[
-        {'top task': {'price': 119.2, 'size':36}, 'timestamp': '2019=02-11 22:06:30.572453', 'top_bid': { 'price': 120.48,  'size': 109}, 'id': '0.109974697771', 'stock':'ABC'},
-        {'top task': {'price': 121.68, 'size':4}, 'timestamp': '2019=02-11 22:06:30.572453', 'top_bid': { 'price': 117.87,  'size': 81}, 'id': '0.109974697771', 'stock':'DEF'}
-    ]
- for quote in quotes:
-        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'],(quote['top_bid']['price'] + quote['top_ask']['price'])/2))
-
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
